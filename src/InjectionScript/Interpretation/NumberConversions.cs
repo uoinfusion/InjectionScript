10463242e3c3704ce9fa257d25ec254aadab4255
Jakub Linhart <jakub.linhart@gmail.com>
2018-11-06 12:07:30 +0100

    Adding more conversions.

--- src/InjectionScript/Interpretation/NumberConversions.cs
+++ src/InjectionScript/Interpretation/NumberConversions.cs
@@ -20,6 +20,18 @@ namespace InjectionScript.Interpretation
             return int.Parse(str, NumberStyles.Integer);
         }
 
+        public static bool TryStr2Int(string str, out int value)
+        {
+            if (str.StartsWith("0x"))
+            {
+                str = str.Substring(2);
+                return int.TryParse(str, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out value);
+            }
+
+            return int.TryParse(str, NumberStyles.Integer, CultureInfo.InvariantCulture, out value);
+        }
+
         public static string Int2Hex(int id) => $"0x{id:X8}";
+        public static string Int2Hex(uint id) => Int2Hex((int)id);
     }
 }
