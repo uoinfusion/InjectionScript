144d69d6df5c008367abd9fa4f96a083a69fc687
Jakub Linhart <jakub.linhart@gmail.com>
2019-04-15 09:16:13 +0200

    Added locking and add/remove object events.
    Better integration with UI. Maybe a little bit too heavyweight - locking is required anyway - even multiple scripts can access Objects from different threads.

--- src/InjectionScript/Runtime/Objects.cs
+++ src/InjectionScript/Runtime/Objects.cs
@@ -10,22 +10,84 @@ namespace InjectionScript.Runtime
     public class Objects : IEnumerable<KeyValuePair<string, int>>
     {
         private readonly Dictionary<string, int> objects = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+        private readonly object objectsLock = new object();
 
-        public void Set(string name, int value) => objects[name] = value;
-        public int Get(string name) => objects[name];
-        public void Clear() => objects.Clear();
+        public event Action<string> KeyAdded;
+        public event Action<string> KeyRemoved;
 
-        public bool TryGet(string name, out int value) => objects.TryGetValue(name, out value);
-        public void Remove(string name) => objects.Remove(name);
+        public void Set(string name, int value)
+        {
+            bool notify = true;
+
+            lock (objectsLock)
+            {
+                if (!objects.ContainsKey(name))
+                {
+                    objects.Add(name, value);
+                    notify = true;
+                }
+                else
+                    objects[name] = value;
+            }
+
+            if (notify)
+                KeyAdded?.Invoke(name);
+        }
+
+        public int Get(string name)
+        {
+            lock (objectsLock)
+            {
+                return objects[name];
+            }
+        }
+
+        public void Clear()
+        {
+            lock (objectsLock)
+            {
+                objects.Clear();
+            }
+        }
+
+        public bool TryGet(string name, out int value)
+        {
+            lock (objectsLock)
+            {
+                return objects.TryGetValue(name, out value);
+            }
+        }
+
+        public void Remove(string name)
+        {
+            bool notify = false;
+            lock (objectsLock)
+            {
+                if (objects.ContainsKey(name))
+                {
+                    notify = true;
+                    objects.Remove(name);
+                }
+            }
+
+            if (notify)
+                KeyRemoved?.Invoke(name);
+        }
 
         public IEnumerator<KeyValuePair<string, int>> GetEnumerator()
         {
-            return objects.GetEnumerator();
+            lock (objectsLock)
+            {
+                return objects.GetEnumerator();
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return objects.GetEnumerator();
+            lock (objectsLock)
+            {
+                return objects.GetEnumerator();
+            }
         }
     }
 }
